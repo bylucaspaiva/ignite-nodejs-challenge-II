88fd95352ec1293ac84939deed5a0776a191f5a3
bylucaspaiva <lucasp4iva@gmail.com>
2022-04-23 23:12:10 -0300

    feat: middlware check todo exist

--- src/index.js
+++ src/index.js
@@ -52,17 +52,29 @@ function checksTodoExists(request, response, next) {
     return response.status(400).json({error: "Invalid id!"});
   }
 
-  const todoIdValidate = userExist.todos.some(todo => todo.id === id);
+  const todoIdValidate = userExist.todos.find(todo => todo.id === id);
   
   if (!todoIdValidate) {
     return response.status(400).json({error: "Invalid todo!"})
   }
 
+  request.todo = todoIdValidate;
+
   next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
